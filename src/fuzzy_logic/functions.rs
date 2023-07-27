d51e4fefaf3c85eebe48018d1af183036915e8a6
Jérôme Chételat <Nanoxium>
2023-07-27 17:23:41 +0200

    Added example and test in membership functions

--- src/fuzzy_logic/functions.rs
+++ src/fuzzy_logic/functions.rs
@@ -1,12 +1,12 @@
 /// Trapezoidal distribution function
 pub fn triangular(a: f64, b: f64, c: f64) -> Box<dyn Fn(f64) -> f64> {
     Box::new(move |x| {
-        if x <= a || x >=c {
+        if x <= a || x >= c {
             0.0
         } else if x <= b {
-            (x-a) / (b-a)
+            (x - a) / (b - a)
         } else {
-            (c - x) / (c -b)
+            (c - x) / (c - b)
         }
     })
 }
@@ -32,11 +32,30 @@ pub fn gaussian(c: f64, sigma: f64) -> Box<dyn Fn(f64) -> f64> {
 }
 
 /// Sigmoid function to evaluate an x
+/// `a` controls the curve steepness and `c` shifts horizontaly the sigmoid.
+/// For a standard sigmoid curve call like this :
+/// ```
+/// # use rtee::prelude::*;
+/// # fn main() {
+/// let sigmoid_func = sigmoid(1.0, 0.0);
+/// assert_eq!(sigmoid_func(0.0), 0.5)
+/// # }
+/// ```
 pub fn sigmoid(a: f64, c: f64) -> Box<dyn Fn(f64) -> f64> {
     Box::new(move |x| 1.0 / (1.0 + (-a * (x - c)).exp()))
 }
 
-/// Bell distribution 
+/// Bell distribution
 pub fn bell(a: f64, b: f64, c: f64) -> Box<dyn Fn(f64) -> f64> {
     Box::new(move |x| 1.0 / (1.0 + ((x - c) / a).abs().powf(2.0 * b)))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sigmoid() {
+        assert_eq!(sigmoid(1.0, 0.0)(0.0), 0.5);
+    }
 }
